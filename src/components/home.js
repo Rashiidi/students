76ec928f78ec0046afeabfd9c21303b2b33b8e97
Rashka <abdirashid@gmail.com>
2024-10-03 22:45:36 +0300

    created navbar

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeComponent = () => {
+  return (
+    <div>
+      <h1>Welcome to Student Info App!</h1>
+    </div>
+  );
+};
+
+export default HomeComponent;
